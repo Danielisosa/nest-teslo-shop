2b4be0b6f3e7dc00e53f8e20e07b8de749db728a
danieli sosa <danielisosa.0512@gmail.com>
2026-09-05 10:30:52 -0400

    Fix Swagger docs route for /api users

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Cambiar la ruta de Swagger para que no colisione con el prefijo global 'api'
+  SwaggerModule.setup('docs', app, document);
 
 
   await app.listen(process.env.PORT);
